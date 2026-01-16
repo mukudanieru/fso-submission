22b7e0485eedd5527db1dff2bf18dd91305c43e1
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2026-01-16 17:33:54 +0800

    finish part1.b - Finish part 1.3 - 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -32,7 +32,7 @@ const Footer = ({ parts }) => {
       <p>
         Number of exercises{" "}
         {parts.reduce((accumulator, item) => {
-          return accumulator + item.exercise;
+          return accumulator + item.exercises;
         }, 0)}
       </p>
     </>
@@ -40,26 +40,30 @@ const Footer = ({ parts }) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
-  const parts = [
-    { name: part1, exercise: exercises1 },
-    { name: part2, exercise: exercises2 },
-    { name: part3, exercise: exercises3 },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <>
       <div>
-        <Header course={course} />
-        <Content parts={parts} />
-        <Footer parts={parts} />
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Footer parts={course.parts} />
       </div>
     </>
   );
